0abc5f6fb3a054e92c6ea85b0e57092f1a29fa71
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2021-07-16 16:57:55 +1000

    Update the navigation on MenuItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import Homepage from "./pages/Hompage";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+const HatPage = () => {
+    return <h1>This is a Hat page</h1>
+}
 function App() {
-  return <div className="App">Welcome to me </div>;
+    return <Router>
+        <Switch>
+            <Route exact path='/' component={Homepage}/>
+            <Route exat path='/shop/hats' component={HatPage} />
+        </Switch>
+    </Router>
 }
 
 export default App;
