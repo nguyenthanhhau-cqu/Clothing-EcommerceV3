6d8f4115fa6767dd892ae4ec4cff058027a439ad
Nguyen Hau <thanhhau.nguyen@cqumail.com>
2021-09-02 17:58:33 +1000

    delete context file

--- client/src/context/cart-context/CartContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const CartContext = createContext({
-  hidden: true,
-  setToggle: () => {},
-});
-
-export default CartContext;
--- client/src/context/collections/CollectionContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import SHOP_DATA from "./SHOP_DATA";
-
-const CollectionsContext = createContext(SHOP_DATA);
-
-export default CollectionsContext;
